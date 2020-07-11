0040d27aa64a17ab399699e0d06b6c136d3283cb
Ramón Carrillo <ramacarr@espol.edu.ec>
2020-07-11 03:45:14 -0500

    Navigation and skeleton

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
-import { Button } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
+import { Layout, Menu } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Info from './pages/Info';
+import Calc from './pages/Calc';
+import Contact from './pages/Contact';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Button type="primary">Hello world</Button>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['home']}>
+            <Menu.Item key="home"><Link to="/">Inicio</Link></Menu.Item>
+            <Menu.Item key="info"><Link to="/info/">Descripción</Link></Menu.Item>
+            <Menu.Item key="calc"><Link to="/calc/">Calculadora</Link></Menu.Item>
+            <Menu.Item key="contact"><Link to="/contact/">Contáctenos</Link></Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '50px' }}>
+          <div className="site-layout-content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/info/" component={Info} />
+              <Route exact path="/calc/" component={Calc} />
+              <Route exact path="/contact/" component={Contact} />
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>©2020</Footer>
+      </Layout>
+    </Router>
   );
 }
 
---
+++ src/pages/Contact.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Form, Input, InputNumber, Button } from 'antd';
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const validateMessages = {
+  required: '${label} is required!',
+  types: {
+    email: '${label} is not validate email!',
+    number: '${label} is not a validate number!',
+  },
+};
+
+export default () => {
+  const onFinish = values => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+      <Form.Item
+        name={['user', 'name']}
+        label="Name"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item
+        name={['user', 'email']}
+        label="Email"
+        rules={[
+          {
+            type: 'email',
+            required: true,
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item name={['user', 'introduction']} label="Introduction">
+        <Input.TextArea />
+      </Form.Item>
+      <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
