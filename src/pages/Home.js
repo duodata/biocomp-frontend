1867dcd9318cd238d4a0a314032e4e4ff43156b5
karinaortega1507 <krortega@espol.edu.ec>
2020-07-17 15:59:38 -0500

    Se rrealizan cambios en la pagina Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 export default () => (
   <div>
-      Home
+    <h1>¿Qué es Balance Iónico?</h1>
+    <section>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
+        laborum omnis dolorum natus magnam iusto vero incidunt optio id,
+        recusandae harum eos minima autem at porro sit sint expedita ullam!
+      </p>
+    </section>
+    <img src="imgBalance.jpg" alt="imgBalance" />
+    <h1>¿Por qué es importante?</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae veniam,
+      nihil harum odio, dolorum nisi cumque ex quidem explicabo placeat ipsam
+      quisquam? Natus fugiat incidunt esse sequi consequuntur aliquam a.
+    </p>
   </div>
 );
